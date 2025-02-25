da4316faa0213c5379a37ec957511a7a91c8822d
LuddeP1g <luddestor5@gmail.com>
2025-02-25 09:35:01 +0100

    added some functions for the game

--- scripts.js
+++ scripts.js
@@ -1,3 +1,25 @@
+let deckid = 0;
+let cardsLeft = 0;
+
+
 function newGame() {
-    console.log("New Game");
+    loadGame();
+    document.getElementById("startScreen").style.display = "none";
+    document.getElementById("gameScreen").style.display = "block";
 }
+
+async function loadGame() {
+    let response = await fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1");
+    let data = await response.json();
+    deckid = data.deck_id;
+    cardsLeft = data.remaining;
+}
+
+function checkRemaining() {
+    alert("Cards remaining: " + cardsLeft);
+}
+
+async function drawCards() {
+
+}
+
